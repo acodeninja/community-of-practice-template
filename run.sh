f0c835ae31db1a4e0c9cd68e873b44d12f96e158
Lawrence <lawrence@acode.ninja>
2023-06-08 12:30:03 +0100

    Better handling of html tidy

--- run.sh
+++ run.sh
@@ -5,7 +5,11 @@ TASK=$1
 case $TASK in
 'build')
   mkdocs build --config-file mkdocs.production.yml
-  tidy -m site/**/*.html
+  tidy \
+    -indent --indent-spaces 2 --indent-attributes no \
+    --wrap-attributes no -wrap 0 \
+    -quiet --show-errors 0 --show-warnings 0 \
+    -m ./**/*.html || echo "Tidied with errors"
   ;;
 'dev')
   mkdocs serve -a 0.0.0.0:8000
